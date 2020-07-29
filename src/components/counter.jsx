a3d6fedacba88b2d442317af19522823d8228357
Noman Shoukat <nomanaadma@gmail.com>
2020-07-29 15:25:30 +0500

    Conditional classes and methods

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,19 +6,26 @@ class Counter extends Component {
         count: 0
     };
 
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
-
     render() { 
         return (
             <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count == 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+
 }
  
 export default Counter;
