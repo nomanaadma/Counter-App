2d70c375d3d32ef9d9d95cb3588319a6e80378fe
Noman Shoukat <nomanaadma@gmail.com>
2020-07-29 14:32:28 +0500

    Expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    
+    state = {
+        count: 0
+    };
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+            <div>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>
+            </div>
+        );
     }
 }
  
