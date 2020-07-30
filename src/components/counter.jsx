abbc69fae2b73a98bdcd4765fd5b8bf0e75db990
Noman Shoukat <nomanaadma@gmail.com>
2020-07-31 02:08:29 +0500

    Delete and Reset Method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Counter extends Component {
     
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
     };
 
     handleIncrement = () => {
-        this.setState({ value: this.state.value+1 })
+        this.setState({ value: this.state.counter.value+1 })
     }
 
     render() { 
@@ -15,6 +15,7 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-small m-2">Delete</button>
             </div>
         );
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
-import Counter from './counter'
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-    state = { 
+    state = {
         counters: [
             { id: 1, value: 0 },
             { id: 2, value: 4 },
             { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
+            { id: 4, value: 0 },
+        ],
+    };
+
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter((c) => c.id !== counterId);
+        this.setState({ counters });
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map((c) => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
     }
-    render() { 
-        return ( 
+
+    render() {
+        return (
             <div>
-                {
-                    this.state.counters.map(counter => (
-                        <Counter key={counter.id} value={counter.value} />
-                    ))
-                }
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                {this.state.counters.map((counter) => (
+                    <Counter
+                        key={counter.id}
+                        onDelete={this.handleDelete}
+                        counter={counter}
+                    />
+                ))}
             </div>
         );
     }
 }
- 
+
 export default Counters;
